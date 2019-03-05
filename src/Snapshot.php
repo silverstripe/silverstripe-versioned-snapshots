d79ebd69ccc2823d4ff404d756f131c12b323607
Aaron Carlino <unclecheese@leftandmain.com>
2019-03-05 16:56:56 +1300

    Linting

---
+++ src/Snapshot.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace SilverStripe\Snapshots;
+
+use SilverStripe\ORM\DataObject;
+use SilverStripe\Security\Member;
+use SilverStripe\Security\Permission;
+
+class Snapshot extends DataObject
+{
+    private static $db = [
+        'OriginHash' => 'Varchar(64)',
+    ];
+
+    private static $has_one = [
+        'Origin' => DataObject::class,
+        'Author' => Member::class,
+    ];
+
+    private static $has_many = [
+        'Items' => SnapshotItem::class,
+    ];
+
+    private static $indexes = [
+        'OriginHash' => true,
+    ];
+
+    private static $table_name = 'VersionedSnapshot';
+
+    private static $singular_name = 'Snapshot';
+
+    private static $plural_name = 'Snapshots';
+
+    private static $default_sort = 'ID ASC';
+
+    public function canCreate($member = null, $context = [])
+    {
+        return Permission::checkMember($member, 'CMS_ACCESS_CMSMain');
+    }
+
+    public function canEdit($member = null, $context = [])
+    {
+        return Permission::checkMember($member, 'CMS_ACCESS_CMSMain');
+    }
+
+    public function canDelete($member = null, $context = [])
+    {
+        return Permission::checkMember($member, 'CMS_ACCESS_CMSMain');
+    }
+
+    public function canView($member = null, $context = [])
+    {
+        return true;
+    }
+
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+        $this->OriginHash = static::hash($this->OriginClass, $this->OriginID);
+    }
+}
---
+++ src/SnapshotItem.php
@@ -0,0 +1,107 @@
+<?php
+
+
+namespace SilverStripe\Snapshots;
+
+use SilverStripe\ORM\DataObject;
+use SilverStripe\Security\Member;
+use SilverStripe\Security\Permission;
+use SilverStripe\Security\Security;
+use SilverStripe\Versioned\ChangeSet;
+use SilverStripe\Versioned\Versioned;
+
+class SnapshotItem extends DataObject
+{
+    private static $db = [
+        'Version' => 'Int',
+        'WasPublished' => 'Boolean',
+        'WasDraft' => 'Boolean',
+        'WasDeleted' => 'Boolean',
+        'ObjectHash' => 'Varchar(64)',
+    ];
+
+    private static $has_one = [
+        'Snapshot' => Snapshot::class,
+        'Object' => DataObject::class,
+        'LinkedFromObject' => DataObject::class,
+        'LinkedToObject' => DataObject::class,
+    ];
+
+    private static $indexes = [
+        'Version' => true,
+        'ObjectHash' => true,
+    ];
+
+    private static $table_name = 'VersionedSnapshotItem';
+
+    private static $singular_name = 'SnapshotItem';
+
+    private static $plural_name = 'SnapshotItems';
+
+    private static $default_sort = 'ID ASC';
+
+
+    public function canView($member = null)
+    {
+        return $this->can(__FUNCTION__, $member);
+    }
+
+    public function canEdit($member = null)
+    {
+        return $this->can(__FUNCTION__, $member);
+    }
+
+    public function canCreate($member = null, $context = [])
+    {
+        return $this->can(__FUNCTION__, $member, $context);
+    }
+
+    public function canDelete($member = null)
+    {
+        return $this->can(__FUNCTION__, $member);
+    }
+
+    /**
+     * Default permissions for this ChangeSetItem
+     *
+     * @param string $perm
+     * @param Member $member
+     * @param array $context
+     * @return bool
+     */
+    public function can($perm, $member = null, $context = [])
+    {
+        if (!$member) {
+            $member = Security::getCurrentUser();
+        }
+
+        // Allow extensions to bypass default permissions, but only if
+        // each change can be individually published.
+        $extended = $this->extendedCan($perm, $member, $context);
+        if ($extended !== null) {
+            return $extended;
+        }
+
+        // Default permissions
+        return (bool)Permission::checkMember($member, ChangeSet::config()->get('required_permission'));
+    }
+
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+        $this->ObjectHash = md5($this->ObjectClass . $this->ObjectID);
+    }
+
+    /**
+     * @return DataObject
+     */
+    public function getItem()
+    {
+        return Versioned::get_version($this->ObjectClass, $this->ObjectID, $this->Version);
+    }
+
+    public function getItemTitle()
+    {
+        return $this->getItem()->singular_name() . '    --  ' . $this->getItem()->getTitle();
+    }
+}
