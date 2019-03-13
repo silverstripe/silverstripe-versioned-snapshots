87beb533e0f51993d509b1813c40f31929cdc835
Aaron Carlino <unclecheese@leftandmain.com>
2019-03-13 16:59:27 +1300

    README

--- src/SnapshotSiteTreeExtension.php
+++ src/SnapshotSiteTreeExtension.php
@@ -3,14 +3,22 @@
 
 namespace SilverStripe\Snapshots;
 
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Forms\HeaderField;
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\Forms\FieldList;
 
 class SnapshotSiteTreeExtension extends DataExtension
 {
-    public function updateCMSFields($fields)
+    public function updateCMSFields(FieldList $fields)
     {
         if (!$this->owner->hasExtension(SnapshotPublishable::class)) {
             return;
+        }
+        /* @var SnapshotPublishable|SiteTree $owner*/
+        $owner = $this->owner;
+        if ($owner->hasOwnedModifications()) {
+            $fields->unshift(HeaderField::create('This page has owned modifications'));
         }
     }
 }
