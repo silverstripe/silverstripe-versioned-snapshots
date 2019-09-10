894d05dc07ceb51bb601003d14afd5474812c3cd
Bernard Hamlin <bernie@silverstripe.com>
2019-09-10 12:32:37 +1200

    Inherited ManyMany through unit test

--- tests/SnapshotTest/GalleryImageJoin.php
+++ tests/SnapshotTest/GalleryImageJoin.php
@@ -1,13 +1,11 @@
 <?php
 
-
 namespace SilverStripe\Snapshots\Tests\SnapshotTest;
 
 use SilverStripe\Dev\TestOnly;
-use SilverStripe\ORM\DataObject;
 use SilverStripe\Versioned\Versioned;
 
-class GalleryImageJoin extends DataObject implements TestOnly
+class GalleryImageJoin extends BaseJoin implements TestOnly
 {
     private static $has_one = [
         'Gallery' => Gallery::class,
