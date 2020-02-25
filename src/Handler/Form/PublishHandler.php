2349dfc46a7148c669035047990ede71c5a4a4f0
Aaron Carlino <unclecheese@leftandmain.com>
2020-02-25 16:22:20 +1300

    Force refresh of publish version

--- src/Handler/Form/PublishHandler.php
+++ src/Handler/Form/PublishHandler.php
@@ -5,6 +5,7 @@ namespace SilverStripe\Snapshots\Handler\Form;
 
 use SilverStripe\EventDispatcher\Event\EventContextInterface;
 use SilverStripe\Forms\Form;
+use SilverStripe\ORM\DataObject;
 use SilverStripe\Snapshots\Snapshot;
 use SilverStripe\Snapshots\SnapshotHasher;
 
@@ -22,10 +23,17 @@ class PublishHandler extends Handler
         if (!$record) {
             return null;
         }
+        // Refresh the record so we get the new version
+        $record = DataObject::get_by_id(get_class($record), $record->ID, false);
+        if (!$record) {
+            return null;
+        }
+
         foreach ($snapshot->Items() as $item) {
             // If it's the origin item, set published state.
             if (static::hashSnapshotCompare($item->getItem(), $record)) {
                 $item->WasPublished = true;
+                $item->Version = $record->Version;
             }
         }
 
