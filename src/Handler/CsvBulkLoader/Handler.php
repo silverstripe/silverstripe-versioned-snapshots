538dbdb36a87bfdc01cb24d202127758e744eb91
Ingo Schommer <me@chillu.com>
2021-05-14 09:03:42 +1200

    NEW CsvBulkLoader integration
    New snapshot for every row that's actually been created or changed.
    Ideally we'd use createSnapshotEvent('CSV Import from <file>', [$objs]),
    but that can be very resource intensive on bulk imports (PHP memory).
    The bulk loader destroys the DataObject instances after loading them,
    so we'd need to recreate them.
    
    We could allow createSnapshot() to accept a map of "class => ID"
    instead, and then recreate+destroy the DataObject instances on the fly.
    For now, recording one snapshot per row is good enough.

---
+++ src/Handler/CsvBulkLoader/Handler.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace SilverStripe\Snapshots\Handler\CsvBulkLoader;
+
+use SilverStripe\EventDispatcher\Event\EventContextInterface;
+use SilverStripe\Snapshots\Handler\HandlerAbstract;
+use SilverStripe\Snapshots\Snapshot;
+
+class Handler extends HandlerAbstract
+{
+    /**
+     * @param EventContextInterface $context
+     * @return Snapshot|null
+     * @throws ValidationException
+     */
+    protected function createSnapshot(EventContextInterface $context): ?Snapshot
+    {
+        $obj = $context->get('obj');
+
+        if (!$obj) {
+            throw new \InvalidArgumentException('Requires "obj" in context');
+        }
+
+        // Create an individual snapshot for each object to ensure they're all captured.
+        // Unlike a recursive publish, with imports we rely on all objects noting the action in their history.
+        // The disadvantage of this approach is that the origin of the modification (a CSV import) isn't recorded.
+        return Snapshot::singleton()->createSnapshot($obj);
+    }
+}
